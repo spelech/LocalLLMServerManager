b63a27aa9c21bb59be36c1be3dde6998c3650573
Steven Pelech <steven.pelech@gmail.com>
2026-08-12 00:36:07 -0500

    fix(wasm): configure browser-wasm target, generate dotnet.js runtime, and entrypoint main.js loader

---
+++ wwwroot/main.js
@@ -0,0 +1,13 @@
+import { dotnet } from './_framework/dotnet.js';
+
+const is_browser = typeof window != "undefined";
+if (!is_browser) {
+    throw new Error(`Expected to be running in a browser`);
+}
+
+const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
+    .withDiagnosticTracing(false)
+    .withApplicationArgumentsFromQuery()
+    .create();
+
+await dotnet.run();
