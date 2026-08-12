634a5c1923a3db2f92f3b9751f1f9eb93021a210
Steven Pelech <steven.pelech@gmail.com>
2026-08-12 01:49:30 -0500

    chore(version): bump version across projects and scripts to v3.4.0

--- Endpoints/HealthEndpoints.cs
+++ Endpoints/HealthEndpoints.cs
@@ -23,7 +23,7 @@ public static class HealthEndpoints
                 StableDiffusion = forgeHealthy ? "Online" : "Offline",
                 ComfyUI = comfyHealthy ? "Online" : "Offline",
                 PreferredImageEngine = settings.PreferredImageEngine,
-                Version = "3.3.0"
+                Version = "3.4.0"
             });
         });
     }
